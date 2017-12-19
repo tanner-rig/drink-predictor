a17e9efeefbebe1366dad4ef205ac7e0fa1d5ea7
Ty Ravsten <tyravsten@gmail.com>
2017-12-19 01:06:22 -0700

    Successfully added Tableau story to FizzFinder. I think we could fix/change our story to be more interesting if needed. I didnt because for some reason the data was not exporting correctly. I can talk more about this later...

--- src/components/tableau/tableau.js
+++ src/components/tableau/tableau.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
 
+var tableauScriptUrl = "http://public.tableau.com/javascripts/api/tableau-2.0.0.min.js";
+
+function load(url) {
+  return new Promise(function(resolve, reject) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = url;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  })
+}
+
+var workbook, activesheet, viz;
+var filterState;
+
+function initViz() {
+  load(tableauScriptUrl)
+    .then(function() {
+      console.log('Successfully loaded Tableau Javascript API!');
+      var containerDiv = document.getElementById("vizContainer"),
+      // var containerDiv = document.createElement("div"),
+      url = "https://public.tableau.com/views/drink-predictor/FizzFindertheStoryBehindUs?:embed=y&:display_count=no";
+      // url = "https://public.tableau.com/views/TableauPractice_51/Sheet3?:embed=y&:display_count=yes&publish=yes";
+
+      containerDiv.className="vizualization";
+
+      var options = {
+        hideTabs: true,
+        hideToolbar: true,
+        onFirstInteractive: function() {
+          listenToMarksSelection();
+          workbook = viz.getWorkbook();
+          activeSheet = workbook.getActiveSheet();
+        }
+      };
+
+      return new tableau.Viz(containerDiv, url);
+    })
+    .catch(function(err) {
+      console.error('Something went wrong loading Tableau Javascript API!', err);
+    })
+}
+
 export default class Tableau extends Component {
+
   render() {
     return (
       <div className="tableau">
-        <h1>Tableau Story displayed here!</h1>
+        <div id="vizContainer" onload={initViz()}></div>
       </div>
     );
   }
